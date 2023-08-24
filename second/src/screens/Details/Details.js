58b75b1deb4ddc836d67e616ab9472d1921234d6
Oğuzhan Varlı <106310128+oguzhanvarli@users.noreply.github.com>
2023-08-24 20:43:11 +0300

    24/08 1. ders sonu

--- second/src/screens/Details/Details.js
+++ second/src/screens/Details/Details.js
@@ -1,19 +1,66 @@
-import { StyleSheet, Text, View, FlatList } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, FlatList, Image, ActivityIndicator } from 'react-native'
+import React, { useEffect, useState } from 'react'
 import HandleNavigate from '../../utilities/HandleNavigate'
+import { Avatar, Button, Card, Title, Paragraph } from 'react-native-paper';
+import axios from 'axios'
+import Swiper from 'react-native-swiper';
 
 export default function Details({ route }) {
 
-  let {item} = route.params
+  let { item } = route.params
+
+  const [product, setProduct] = useState({
+    images: []
+  })
+
+  useEffect(() => {
+    getProductById()
+  }, [])
+
+
+  const getProductById = () => {
+    axios.get(`https://dummyjson.com/products/${item.id}`)
+      .then(res => setProduct(res.data))
+      .catch(error => console.log('Get By Id Product Error' + error))
+  }
 
   return (
-    <View>
-      <Text>{item.title}</Text>
-      <Text>{item.brand}</Text>
-      <Text>{item.rating}</Text>
-      <Text>{item.stock}</Text>
-    </View>
+
+    <>
+      <View>
+        <Text style={styles.title}>{product.title}</Text>
+        <Text style={styles.title}>{product.brand}</Text>
+      </View>
+      <Swiper style={styles.wrapper} height={400} horizontal={true} autoplay>
+        {product.images.length > 0 ? product.images.map((image, key) => (
+          <View key={key} style={styles.slide}>
+            <Image style={styles.image} source={{ uri: `${image}` }} />
+          </View>
+        )) : <ActivityIndicator size="large" color="#0000ff" />}
+      </Swiper>
+      <Text>{product.description}</Text>
+      <Button>Add To Card</Button>
+
+    </>
+
+
+
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  text: {
+    margin: 10
+  },
+  wrapper: {},
+  slide: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: 'transparent'
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+    flex: 1
+  }
+})
