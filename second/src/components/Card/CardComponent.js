42da285088aef90cab8bdae47bc793d05674ac4d
Oğuzhan Varlı <106310128+oguzhanvarli@users.noreply.github.com>
2023-08-27 09:45:28 +0300

    27/08 table added.

--- second/src/components/Card/CardComponent.js
+++ second/src/components/Card/CardComponent.js
@@ -1,37 +1,101 @@
-import { StyleSheet, Text, View, Image } from 'react-native'
-import React from 'react'
+// import { StyleSheet, Text, View, Image } from 'react-native'
+// import React from 'react'
 
-export default function CardComponent({item}) {
-    
+// export default function CardComponent({ item }) {
+
+
+//   return (
+//     <View style={styles.container}>
+//       <View style={styles.secondContainer}>
+//         <Image style={styles.tinyLogo} source={{
+//           uri: item.thumbnail,
+//         }} />
+//       </View>
+//       <View style={styles.secondContainer2}>
+//         <Text style={styles.title}>{item.title}</Text>
+//         <Text style={styles.brand}>{item.brand}</Text>
+//       </View>
+//       <View style={styles.secondContainer3}>
+//         <Text style={styles.price}>{item.price}$</Text>
+//       </View>
+//     </View>
+//   )
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     // flex: 1,
+//     flexDirection: 'row',
+//     justifyContent: 'space-between',
+//     margin: 10,
+//     alignItems: 'center'
+//   },
+//   secondContainer: {
+//     flex: 1
+//   },
+//   secondContainer2: {
+//     flex: 2,
+//     justifyContent: 'center'
+//   },
+//   secondContainer3: {
+//     flex: 1
+//   },
+//   tinyLogo: {
+//     width: 80,
+//     height: 80,
+//   },
+//   price : {
+//     fontSize: 20,
+//     color: '#006400'
+//   },
+//   title: {
+//     fontSize: 16,
+//     textAlign: 'center'
+//   },
+//   brand: {
+//     textAlign: 'center',
+//     color: '#DC143C'
+//   }
+// })
+
+
+import * as React from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native'
+import { DataTable } from 'react-native-paper';
+
+const CardComponent = ({ item }) => {
+
   return (
-    <View style={styles.container}>
-      <View style={styles.secondContainer}>
-            <Image style={styles.tinyLogo} source={{
-          uri: item.thumbnail,
-        }} />
-      </View>
-      <View style={styles.secondContainer}>
-        <Text>{item.title}</Text>
-        <Text>{item.brand}</Text>
-      </View>
-      <View style={styles.secondContainer}>
-        <Text>{item.price}</Text>
-      </View>
-      <View style={styles.secondContainer}></View>
-    </View>
-  )
-}
+    <DataTable.Row style={styles.row}>
+      <DataTable.Cell> <Image style={styles.tinyLogo} source={{
+        uri: item.thumbnail,
+      }} /></DataTable.Cell>
+      < DataTable.Cell numeric > <View><Text style={styles.title}>{item.title}</Text><Text style={styles.brand}>{item.brand}</Text></View></DataTable.Cell>
+      <DataTable.Cell numeric ><Text style={styles.price}>{item.price}$</Text></DataTable.Cell>
+    </DataTable.Row>
+  );
+};
+
+export default CardComponent;
 
 const styles = StyleSheet.create({
-    container: {
-        // flex: 1,
-        flexDirection: 'row'
-    },
-    secondContainer: {
-        // flex: 1
-    },
-    tinyLogo: {
-        width: 50,
-        height: 50,
+  row: {
+    minHeight: 90,
+  },
+  tinyLogo: {
+    width: 80,
+    height: 80,
+  },
+  price: {
+    fontSize: 20,
+    color: '#006400'
+  },
+  title: {
+        fontSize: 16,
+        textAlign: 'center'
       },
-})
+      brand: {
+        textAlign: 'center',
+        color: '#DC143C'
+      }
+  })
--- second/src/screens/Card/Card.js
+++ second/src/screens/Card/Card.js
@@ -1,18 +1,25 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, ScrollView } from 'react-native'
 import React from 'react'
 import { useSelector } from 'react-redux'
 import CardComponent from '../../components/Card/CardComponent'
+import { DataTable } from 'react-native-paper';
 
 export default function Card() {
 
     const card = useSelector((state) => state.card)
-    console.log(card)
     return (
-        <View>
-            {card.products.map((product, key) => (
-             <CardComponent key={key} item={product} />
-            ))}
-        </View>
+        <ScrollView>
+            <DataTable>
+                <DataTable.Header>
+                    <DataTable.Title>Image</DataTable.Title>
+                    <DataTable.Title numeric>Product</DataTable.Title>
+                    <DataTable.Title numeric>Price</DataTable.Title>
+                </DataTable.Header>
+                {card.products.map((product, key) => (
+                    <CardComponent key={key} item={product} />
+                ))}
+            </DataTable>
+        </ScrollView>
     )
 }
 
